3da16fbacc36cd5e469e579bf0300b24344cef7b
John Reid <jreid@rapid7.com>
2022-12-04 08:27:11 +0000

    part two

--- day04/cleanup.go
+++ day04/cleanup.go
@@ -15,22 +15,28 @@ func main() {
 	}
 	defer file.Close()
 
-	total := 0
+	total1 := 0
+	total2 := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		result := part_one(line)
-		if result {
-			total += 1
+		result1 := part_one(line)
+		if result1 {
+			total1 += 1
 		}
 
-		if result == false {
-			fmt.Printf("%s %s\n", line, "false")
+		result2 := part_two(line)
+		if result2 {
+			total2 += 1
+		}
+		if result2 == false {
+			fmt.Printf("%s .  false\n", line)
 		}
 	}
 
-	fmt.Printf("Found %d overlaps\n", total)
+	fmt.Printf("Found %d fully contained\n", total1)
+	fmt.Printf("Found %d foverlaps\n", total2)
 }
 
 func part_one(line string) bool {
@@ -49,5 +55,25 @@ func part_one(line string) bool {
 	} else if b0 <= a0 && b1 >= a1 {
 		return true
 	}
+	return false
+}
+
+func part_two(line string) bool {
+	pairs := strings.Split(line, ",")
+
+	a := strings.Split(pairs[0], "-")
+	b := strings.Split(pairs[1], "-")
+
+	a0, _ := strconv.Atoi(a[0])
+	a1, _ := strconv.Atoi(a[1])
+	b0, _ := strconv.Atoi(b[0])
+	b1, _ := strconv.Atoi(b[1])
+
+	if a0 >= b0 && a0 <= b1 {
+		return true
+	} else if b0 >= a0 && b0 <= a1 {
+		return true
+	}
+
 	return false
 }
