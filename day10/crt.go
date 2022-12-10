3abdb24779a08012a6a91dcb2f9e7dba6cd03303
John Reid <jreid@rapid7.com>
2022-12-10 09:17:32 +0000

    Part two

--- day10/crt.go
+++ day10/crt.go
@@ -17,7 +17,8 @@ func main() {
 
 	x := 1
 	cycles := 0
-	values := []int{}
+	var values []int
+	screen := [240]int{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		input := strings.Split(scanner.Text(), " ")
@@ -25,14 +26,17 @@ func main() {
 		case "noop":
 			{
 				cycles += 1
-				values = check_cycle(x, cycles, values)
+				values = checkCycle(x, cycles, values)
+				screen = checkScreen(x, cycles, screen)
 			}
 		case "addx":
 			{
 				cycles += 1
-				values = check_cycle(x, cycles, values)
+				values = checkCycle(x, cycles, values)
+				screen = checkScreen(x, cycles, screen)
 				cycles += 1
-				values = check_cycle(x, cycles, values)
+				values = checkCycle(x, cycles, values)
+				screen = checkScreen(x, cycles, screen)
 				val, _ := strconv.Atoi(input[1])
 				x += val
 			}
@@ -44,9 +48,19 @@ func main() {
 
 	signal := sum(values)
 	fmt.Printf("Total strength: %d\n", signal)
+
+	drawScreen(screen)
 }
 
-func check_cycle(x int, cycle int, values []int) []int {
+func checkScreen(x int, cycle int, screen [240]int) [240]int {
+	beamPos := cycle%40 - 1
+	if beamPos >= x-1 && beamPos <= x+1 {
+		screen[cycle-1] = 1
+	}
+	return screen
+}
+
+func checkCycle(x int, cycle int, values []int) []int {
 	if (cycle+20)%40 == 0 {
 		values = append(values, cycle*x)
 	}
@@ -59,4 +73,17 @@ func sum(s []int) int {
 		result += v
 	}
 	return result
+}
+
+func drawScreen(screen [240]int) {
+	for line := 0; line < 6; line++ {
+		for _, pixel := range screen[line*40 : line*40+40] {
+			if pixel == 1 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
+	}
 }
