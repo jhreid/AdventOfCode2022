ebfcc9e12ed70c460a00abd7647d662e0ce0ad41
John Reid <jreid@rapid7.com>
2022-12-11 15:18:21 +0000

    Part two

--- day09/ropebridge.go
+++ day09/ropebridge.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +14,48 @@ type Pos struct {
 	y int
 }
 
+type Knot struct {
+	number    int
+	pos       Pos
+	history   []Pos
+	locations map[Pos]int
+}
+
+func (k *Knot) follow(leader *Knot) {
+	x := leader.pos.x
+	y := leader.pos.y
+	dx := x - k.pos.x
+	dy := y - k.pos.y
+
+	if dy == 0 { // horizontal
+		if dx > 1 {
+			k.pos.x += 1
+		} else if dx < -1 {
+			k.pos.x -= 1
+		}
+	} else if dx == 0 { // vertical
+		if dy > 1 {
+			k.pos.y += 1
+		} else if dy < -1 {
+			k.pos.y -= 1
+		}
+	} else if (math.Abs(float64(dy)) == 2 && math.Abs(float64(dx)) > 0) || (math.Abs(float64(dx)) == 2 && math.Abs(float64(dx)) >= 1) {
+		if dx > 0 {
+			k.pos.x += 1
+		} else {
+			k.pos.x -= 1
+		}
+		if dy > 0 {
+			k.pos.y += 1
+		} else {
+			k.pos.y -= 1
+		}
+	}
+
+	k.history = append(k.history, k.pos)
+	k.locations[k.pos] += 1
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -20,13 +63,25 @@ func main() {
 	}
 	defer file.Close()
 
-	rope := []Pos{{0, 0}, {0, 0}}
-	numberOfKnots := len(rope)
-	locations := make(map[Pos]int)
-	tails := []Pos{}
-	head := rope[0]
-	tail := rope[numberOfKnots-1]
-	locations[tail] = 1
+	tail := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 9}
+	eight := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 8}
+	seven := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 7}
+	six := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 6}
+	five := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 5}
+	four := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 4}
+	three := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 3}
+	two := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 2}
+	one := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 1}
+	head := Knot{pos: Pos{0, 0}, locations: map[Pos]int{}, history: []Pos{}, number: 0}
+
+	//rope := []*Knot{&head, &one, &two, &three, &four, &five, &six, &seven, &eight, &tail}
+	rope := []*Knot{&head, &one, &two, &three, &four, &five, &six, &seven, &eight, &tail}
+
+	for _, k := range rope {
+		k.history = append(k.history, k.pos)
+		k.locations[k.pos] = 1
+	}
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -35,85 +90,30 @@ func main() {
 			direction := instruction[0]
 			distance, _ := strconv.Atoi(instruction[1])
 
-			newPositions := processInstruction(distance, direction, []Pos{head, tail})
-			head = newPositions[0]
-			tail = newPositions[1]
-			for _, t := range newPositions[2:] {
-				locations[t] += 1
-			}
-			tails = append(tails, tail)
-
-			// fmt.Printf("Head position: %v\n", head)
-			// fmt.Printf("Tail position: %v\n", tail)
+			processInstruction2(rope, distance, direction)
 		}
 	}
 
-	fmt.Printf("Positions visited: %d\n", len(locations))
+	fmt.Printf("Positions visited: %d\n", len(rope[9].locations))
 }
 
-func moveKnots(direction string, head Pos, rest []Pos) []Pos {
-	tail := rest[0]
-	switch direction {
-	case "R":
-	case "L":
-	case "U":
-	case "D":
-	}
-	return moveKnots(direction, tail, rest[1:])
-}
-
-func processInstruction(distance int, direction string, rope []Pos) []Pos {
-	var tails []Pos
+func processInstruction2(rope []*Knot, distance int, direction string) {
 	head := rope[0]
-	tail := rope[1]
 	for d := 1; d <= distance; d++ {
-		tx := tail.x
-		ty := tail.y
 		switch direction {
 		case "R":
-			{
-				head = Pos{head.x + 1, head.y}
-				if head.x-tx > 1 {
-					tx += 1
-					if ty != head.y && d > 1 {
-						ty = head.y
-					}
-				}
-			}
-		case "U":
-			{
-				head = Pos{head.x, head.y + 1}
-				if head.y-ty > 1 {
-					ty += 1
-					if tx != head.x && d > 1 {
-						tx = head.x
-					}
-				}
-			}
+			head.pos.x += 1
 		case "L":
-			{
-				head = Pos{head.x - 1, head.y}
-				if head.x-tx < -1 {
-					tx -= 1
-					if ty != head.y && d > 1 {
-						ty = head.y
-					}
-				}
-			}
+			head.pos.x -= 1
+		case "U":
+			head.pos.y += 1
 		case "D":
-			{
-				head = Pos{head.x, head.y - 1}
-				if head.y-ty < -1 {
-					ty -= 1
-					if tx != head.x && d > 1 {
-						tx = head.x
-					}
-				}
-			}
+			head.pos.y -= 1
 		}
-		tail = Pos{tx, ty}
-		tails = append(tails, tail)
-	}
 
-	return append([]Pos{head, tail}, tails...)
+		rope[1].follow(head)
+		for i, k := range rope[2:] {
+			k.follow(rope[i+1])
+		}
+	}
 }
