5b1f4f9d0f68475060f87c275dfb6bceec9aad1a
John Reid <jreid@rapid7.com>
2022-12-14 14:51:58 +0000

    A*

--- day12/climb.go
+++ day12/climb.go
@@ -11,22 +11,6 @@ type Pos struct {
 	y int
 }
 
-type Node struct {
-	p     Pos
-	count int
-}
-
-type Queue []Node
-
-func (q Queue) contains(node Node) bool {
-	for _, n := range q {
-		if n.p == node.p {
-			return true
-		}
-	}
-	return false
-}
-
 func main() {
 	file, err := os.ReadFile("input.txt")
 	if err != nil {
@@ -34,7 +18,6 @@ func main() {
 	}
 	input := strings.Split(string(file), "\n")
 
-	var start Pos
 	var starts []Pos
 	var end Pos
 
@@ -46,9 +29,6 @@ func main() {
 	}
 	for i := range hill {
 		for j := range hill[i] {
-			if hill[i][j] == 'S' {
-				start = Pos{i, j}
-			}
 			if hill[i][j] == 'S' || hill[i][j] == 'a' {
 				starts = append(starts, Pos{i, j})
 			} else if hill[i][j] == 'E' {
@@ -56,12 +36,12 @@ func main() {
 			}
 		}
 	}
-	hill[end.x][end.y] = 'z'
+	hill[end.x][end.y] = 'z' + 1
 
 	shortest := 412
 	for _, start := range starts {
 		hill[start.x][start.y] = 'a'
-		result := breadthFirstSearch(hill, start, end)
+		result := astar(hill, start, end)
 		if result < shortest {
 			shortest = result
 		}
@@ -69,80 +49,23 @@ func main() {
 
 	fmt.Println(shortest)
 
-	astar(hill, Node{start, 0}, Node{end, 0})
 }
 
-func astar(hill [][]byte, start Node, end Node) {
-	queue := Queue{end}
-
-	i := 0
-	for i < len(queue) {
-		current := queue[i]
-		if current.p == start.p {
-			break
-		}
-		neighbours := []Node{{Pos{current.p.x + 1, current.p.y}, i + 1}, {Pos{current.p.x - 1, current.p.y}, i + 1}, {Pos{current.p.x, current.p.y + 1}, i + 1}, {Pos{current.p.x, current.p.y - 1}, i + 1}}
-		println(neighbours)
-
-		for _, n := range neighbours {
-			if n.p.x < 0 || n.p.x >= len(hill) || n.p.y < 0 || n.p.y >= len(hill[0]) {
-				continue
-			}
-			if queue.contains(n) {
-				continue
-			}
-			if (hill[current.p.x][current.p.y] > hill[n.p.x][n.p.y]) && (hill[current.p.x][current.p.y]-hill[n.p.x][n.p.y] > 1) {
-				continue
-			}
-			queue = append(queue, n)
-		}
-
-		i++
-	}
-
-	fmt.Println(i)
-
-	visited := make(map[Pos]string)
-	for _, n := range queue {
-		visited[n.p] = string(hill[n.p.x][n.p.y])
-	}
-	for x := 0; x < len(hill); x++ {
-		for y := 0; y < len(hill[0]); y++ {
-			n, exist := visited[Pos{x, y}]
-			if exist {
-				fmt.Print(n)
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println()
-	}
-
-	fmt.Println()
-	fmt.Println()
-
-	fmt.Println()
-	fmt.Println()
-}
-
-func breadthFirstSearch(hill [][]byte, start Pos, end Pos) int {
+func astar(hill [][]byte, start Pos, end Pos) int {
+	queue := []Pos{end}
 	visited := make(map[Pos]bool)
-	visited[start] = true
-	queue := []Pos{start}
-	steps := 0
-	found := false
+	visited[end] = true
 
+	pathLength := 0
 	for len(queue) > 0 {
-		k := len(queue)
-		for i := 0; i < k; i++ {
-			cur := queue[0]
+		j := len(queue)
+		for i := 0; i < j; i++ {
+			current := queue[0]
 			queue = queue[1:]
-			neighbours := []Pos{{cur.x + 1, cur.y}, {cur.x - 1, cur.y}, {cur.x, cur.y + 1}, {cur.x, cur.y - 1}}
-
-			if cur == end {
-				found = true
-				return steps
+			if current == start {
+				return pathLength
 			}
+			neighbours := []Pos{{current.x + 1, current.y}, {current.x - 1, current.y}, {current.x, current.y + 1}, {current.x, current.y - 1}}
 
 			for _, n := range neighbours {
 				if n.x < 0 || n.x >= len(hill) || n.y < 0 || n.y >= len(hill[0]) {
@@ -151,19 +74,16 @@ func breadthFirstSearch(hill [][]byte, start Pos, end Pos) int {
 				if visited[n] {
 					continue
 				}
-				if (hill[n.x][n.y] > hill[cur.x][cur.y]) && (hill[n.x][n.y]-hill[cur.x][cur.y] > 1) {
+				if (hill[current.x][current.y] > hill[n.x][n.y]) && (hill[current.x][current.y]-hill[n.x][n.y] > 1) {
 					continue
 				}
 				visited[n] = true
 				queue = append(queue, n)
 			}
 		}
-		steps++
+
+		pathLength++
 	}
 
-	if !found {
-		return 500
-	}
-
-	return steps
+	return pathLength
 }
