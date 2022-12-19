7ef78693551cce5a198868d36a40027c6d0472b1
John Reid <jreid@rapid7.com>
2022-12-19 23:04:57 +0000

    part two

--- day18/boiling.go
+++ day18/boiling.go
@@ -61,6 +61,119 @@ func (d Droplet) surfaceArea() int {
 	return area - hiddenSurfaces
 }
 
+func (d Droplet) fillVoids() {
+	maxX := d.maxX()
+	maxY := d.maxY()
+	maxZ := d.maxZ()
+	filledCubes := make(map[Cube]int)
+	for x := 0; x <= maxX; x++ {
+		for y := 0; y <= maxY; y++ {
+			for z := 0; z <= maxZ; z++ {
+				test := Cube{x, y, z}
+				if _, exists := d.cubes[test]; exists {
+					filledCubes[test] = 1
+				}
+			}
+		}
+	}
+
+	filledCubes = floodFill(filledCubes, maxX, maxY, maxZ)
+
+	for x := 0; x <= maxX; x++ {
+		for y := 0; y <= maxY; y++ {
+			for z := 0; z <= maxZ; z++ {
+				test := Cube{x, y, z}
+				if filledCubes[test] == 0 {
+					filledCubes[test] = 2
+				}
+			}
+		}
+	}
+	for x := -1; x <= maxX+1; x++ {
+		for y := -1; y <= maxY+1; y++ {
+			for z := -1; z <= maxZ+1; z++ {
+				test := Cube{x, y, z}
+				if filledCubes[test] == -1 {
+					delete(filledCubes, test)
+				}
+			}
+		}
+	}
+	for x := 0; x <= maxX; x++ {
+		for y := 0; y <= maxY; y++ {
+			for z := 0; z <= maxZ; z++ {
+				test := Cube{x, y, z}
+				if filledCubes[test] == 2 {
+					d.addCube(test)
+				}
+			}
+		}
+	}
+}
+
+func floodFill(filledCubes map[Cube]int, maxX int, maxY int, maxZ int) map[Cube]int {
+	queue := []Cube{{0, 0, 0}}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		if filledCubes[current] == 0 {
+			filledCubes[current] = -1
+			if current.x > 0 {
+				queue = append(queue, Cube{current.x - 1, current.y, current.z})
+			}
+			if current.x < maxX {
+				queue = append(queue, Cube{current.x + 1, current.y, current.z})
+			}
+			if current.y > 0 {
+				queue = append(queue, Cube{current.x, current.y - 1, current.z})
+			}
+			if current.y < maxY {
+				queue = append(queue, Cube{current.x, current.y + 1, current.z})
+			}
+			if current.z > 0 {
+				queue = append(queue, Cube{current.x, current.y, current.z - 1})
+			}
+			if current.z < maxZ {
+				queue = append(queue, Cube{current.x, current.y, current.z + 1})
+			}
+		}
+	}
+
+	return filledCubes
+}
+
+func (d Droplet) maxX() int {
+	x := 0
+	for c := range d.cubes {
+		if c.x > x {
+			x = c.x
+		}
+	}
+	return x
+}
+
+func (d Droplet) maxY() int {
+	y := 0
+	for c := range d.cubes {
+		if c.y > y {
+			y = c.y
+		}
+	}
+	return y
+}
+
+func (d Droplet) maxZ() int {
+	z := 0
+	for c := range d.cubes {
+		if c.z > z {
+			z = c.z
+		}
+	}
+	return z
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -81,4 +194,7 @@ func main() {
 	}
 
 	fmt.Printf("Part one: %d\n\n", droplet.surfaceArea())
+
+	droplet.fillVoids()
+	fmt.Printf("Part two: %d\n\n", droplet.surfaceArea())
 }
